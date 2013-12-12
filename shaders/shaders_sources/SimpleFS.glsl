a24b80141f6ca500ed6788009cc16c3e6a7a0b29
L-Naej <jn.chiganne@gmail.com>
2013-12-12 22:31:42 +0100

    Ajout .gitignore et nettoyage de printemps

--- shaders/shaders_sources/SimpleFS.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-in vec3 FragColor;
-
-out vec4 oFragColor;
-
-void main(void)
-{
-  oFragColor = vec4(FragColor , 1.0f);
-}
--- shaders/shaders_sources/color2d.fs.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-
-in vec3 vFragColor;
-
-out vec3 fFragColor;
-
-void main() {
-   fFragColor = vFragColor;
-};
